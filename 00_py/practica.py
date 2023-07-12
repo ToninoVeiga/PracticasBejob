475605610cd84f3993142db51a7a4c9c8a7ef148
ToninoVeiga <toniveiga16@gmail.com>
2023-07-12 16:05:25 +0200

    V3.0 Versión inicial P.O.O.

--- 00_py/practica.py
+++ 00_py/practica.py
@@ -1,52 +1,46 @@
 import random
 
-def is_valid_integer(string):
-  """
-Verificar que una cadena sea un número entero entre 1 y 20 o sea el literal "x" (sin distinción entre mayúsculas y minúsculas).
-   Entrada:
-     string: La cadena a verificar.
-   Salida:
-     True si la cadena es válida, False en caso contrario.
-     "string" / "integer" : En caso de True indica si se trata de un numero válido o del caracter de finalización
-  """
-  if string.lower() == "x":
-    return True, "string"
-  try:
-    int(string)
-  except ValueError:
-    return False, "string"
-  if int(string) < 1 or int(string) > 20:
-    return False, "string"
-  return True, "integer"
-
-#Bucle principal
-while True:
+class Matrix:
+    def __init__(self, n):
+        self.n = n
+        self.matrix = [[0 for _ in range(n)] for _ in range(n)]
+    # Rellenar matriz con valores enteros aleatorios
+    def generate(self):
+        for i in range(self.n):
+            for j in range(self.n):
+                self.matrix[i][j] = random.randint(0, 9)
+    # Imprimir la matriz
+    def print(self):
+        for row in self.matrix:
+            print(row)
+    # Sumar los valores de cada fila e imprime resultado
+    def add_rows(self):
+        for row in range(len(self.matrix)):
+            sum = 0
+            for element in self.matrix[row]:
+                sum += element
+            print(f"The sum of row", int(row)+1, "is" , {sum} )
+    # Sumar los valores de cada columna e imprime resultado
+    def add_cols(self):
+        for col in range(len(self.matrix[0])):
+            sum = 0
+            for row in range(len(self.matrix)):
+                sum += self.matrix[row][col]
+            print(f"The sum of column" , int(col)+1 , "is" , {sum})
     # Solicitar al usuario el valor de dimensión
-    str_dimension = (input("Introducir dimension de matriz: "))
-    itsok , valor = is_valid_integer(str_dimension)
-    if itsok:
-        if valor == "integer":
-        # Si se ha introducido un integer ok:
-            num_dimension = int(str_dimension) 
-            # Crear la matriz y rellenarla con valores enteros aleatorios
-            matriz = [[random.randint(0, 9) for _ in range(num_dimension)] for _ in range(num_dimension)]
-            # Imprimir la matriz
-            for fila in matriz:
-                print(fila)
-            # Sumar los valores de cada fila e imprimir el número de fila
-            for i, fila in enumerate(matriz, 1):
-                suma_fila = sum(fila)
-                print("Suma de la fila", i, ":", suma_fila)
+    def get_matrix_dimension():
+        while True:
+            dimension = input("Enter the dimension of the matrix: ")
+            try:
+                dimension = int(dimension)
+                if dimension > 0:
+                    return dimension
+            except ValueError:
+                print("Please enter a valid integer.")
 
-            # Sumar los valores de cada columna
-            for columna in range(num_dimension):
-                suma_columna = sum(matriz[fila][columna] for fila in range(num_dimension))
-                print("Suma de la columna ", columna+1 , ":" , suma_columna)
-            print("Cálculo finalizado")
-            break
-        else:
-            print("Proceso finalizado a petición")
-            break
-    else: 
-       print("Enter an integer between 2 or 20 to process matrix or 'x' to exit")
-
+dimension = Matrix.get_matrix_dimension()
+matrix = Matrix(dimension)
+matrix.generate()
+matrix.print()
+matrix.add_rows()
+matrix.add_cols()
